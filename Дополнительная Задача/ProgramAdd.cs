fb3d07ae99634c964fdcec359871ed6cc8b72c14
Dmitriy <dimamylykob@gmail.com>
2023-12-08 13:15:59 +0400

    I'm in the getaway car

--- Дополнительная Задача/ProgramAdd.cs
+++ Дополнительная Задача/ProgramAdd.cs
@@ -1,90 +1,78 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Text;
+using System.Linq;
 
 class Program
 {
-    static int NaiveSearch(string text, string pattern)
-    {
-        int n = text.Length;
-        int m = pattern.Length;
-
-        for (int i = 0; i <= n - m; i++)
-        {
-            int j;
-            for (j = 0; j < m; j++)
-            {
-                if (text[i + j] != pattern[j])
-                    break;
-            }
-            if (j == m)
-                return i;
-        }
-        return -1;
-    }
-
-    static int RabinKarpSearch(string text, string pattern)
-    {
-        int n = text.Length;
-        int m = pattern.Length;
-        int d = 256; // размер алфавита
-        int q = 101; // простое число
-
-        int h = 1;
-        for (int i = 0; i < m - 1; i++)
-            h = (h * d) % q;
-
-        int p = 0; // хеш для pattern
-        int t = 0; // хеш для text
-        for (int i = 0; i < m; i++)
-        {
-            p = (d * p + pattern[i]) % q;
-            t = (d * t + text[i]) % q;
-        }
-
-        for (int i = 0; i <= n - m; i++)
-        {
-            if (p == t)
-            {
-                int j;
-                for (j = 0; j < m; j++)
-                {
-                    if (text[i + j] != pattern[j])
-                        break;
-                }
-                if (j == m)
-                    return i;
-            }
-            if (i < n - m)
-            {
-                t = (d * (t - text[i] * h) + text[i + m]) % q;
-                if (t < 0)
-                    t = (t + q);
-            }
-        }
-        return -1;
-    }
-
     static void Main()
     {
-        string text = File.ReadAllText("D:\\.program\\C#\\SSU_DSA\\Дополнительная Задача\\input.txt", Encoding.UTF8).ToLower();
-        string pattern = "гъп";
+        string sourceString = GenerateRandomString(100000);
+        string searchString = GenerateRandomString(3);
 
         Stopwatch stopwatch = new Stopwatch();
-        int index = 0;
+
+        // Прямой поиск подстроки в строке
         stopwatch.Start();
-        index = NaiveSearch(text, pattern);
+        int index1 = sourceString.IndexOf(searchString);
         stopwatch.Stop();
-        Console.WriteLine("Индекс искомой подстроки: " + index);
-        Console.WriteLine("Время выполнения прямого поиска: " + stopwatch.Elapsed);
+        Console.WriteLine("Время выполнения прямого поиска подстроки в строке: " + stopwatch.Elapsed);
 
         stopwatch.Reset();
 
+        // Алгоритм Карпа-Рабина
         stopwatch.Start();
-        index = RabinKarpSearch(text, pattern);
+        int index2 = RabinKarpAlgorithm(sourceString, searchString);
         stopwatch.Stop();
-        Console.WriteLine("Индекс искомой подстроки: " + index);
         Console.WriteLine("Время выполнения алгоритма Карпа-Рабина: " + stopwatch.Elapsed);
+    }
+
+    static string GenerateRandomString(int length)
+    {
+        Random random = new Random();
+        const string chars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    static int RabinKarpAlgorithm(string t, string s)
+    {
+        const long P = 37;
+        //вычисляем массив степеней P
+        long[] pwp = new long[t.Length];
+        pwp[0] = 1;
+        for (int i = 1; i < t.Length; i++)
+        {
+            pwp[i] = pwp[i - 1] * P;
+        }
+        //вычисляем массив хэш-значение для всех префиксов строки T
+        long[] h = new long[t.Length];
+        for (int i = 0; i < t.Length; i++)
+        {
+            h[i] = (t[i] - 'a' + 1) * pwp[i]; //1
+            if (i > 0)
+                h[i] += h[i - 1];
+        }
+        // вычисляем хэш-значение для подстроки S
+        long h_s = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            h_s += (s[i] - 'a' + 1) * pwp[i];
+        }
+        //проводим поиск по хеш-значениям
+        for (int i = 0; i + s.Length - 1 < t.Length; i++)
+        {
+            // находим хэш-значение подстроки T начиная с позиции i длиною s.Length
+            long cur_h = h[i + s.Length - 1];
+            if (i > 0)
+            {
+                cur_h -= h[i - 1];
+            }
+            //приводим хэш-значения двух подстрок к одной степени
+            if (cur_h == h_s * pwp[i]) // если хеш-значения равны, то и подстроки равны
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
